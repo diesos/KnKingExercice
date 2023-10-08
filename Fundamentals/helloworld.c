0032251b78cb7f973d017a0bbe0f8065b76ac1c2
Diesos <ozturk@live.Fr>
2023-10-08 10:41:44 +0200

    Exercices

---
+++ Fundamentals/helloworld.c
@@ -0,0 +1,13 @@
+/*****************************
+*Programming book by K.n King*
+* Exercices                  *
+* 08/10/2023 by diesos       *
+*****************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World.\n");
+	return 0;
+}
